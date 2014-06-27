59e0af1a6890b1198ba0b6cf5ff80854cd78289c
muli <muli@cs.cmu.edu>
2014-06-27 17:31:33 +0800

    use --disable-fortran when compiling mpich on mac

--- install.sh
+++ install.sh
@@ -14,27 +14,30 @@ tar -zxf zeromq-4.0.4.tar.gz
 tar -zxf zlib-1.2.8.tar.gz
 tar -zxf snappy-1.1.2.tar.gz
 tar -zxf gtest-1.7.0.tar.gz
-tar -zxf mpich-3.1.1.tar.gz 
+tar -zxf mpich-3.1.1.tar.gz
 tar -zxf cityhash-1.1.1.tar.gz
 
-cd protobuf* && ./configure -prefix=$prefix && make && make install && cd ..
-cd gflags* && ./configure -prefix=$prefix && make && make install && cd ..
-cd glog* && ./configure -prefix=$prefix && make && make install && cd ..
-cd zeromq* && ./configure -prefix=$prefix && make && make install && cd ..
-cd zlib* && ./configure -prefix=$prefix && make && make install && cd ..
-cd snappy* && ./configure -prefix=$prefix && make && make install && cd ..
-cd mpich* && ./configure -prefix=$prefix && make && make install && cd ..
-cd cityhash* && ./configure -prefix=$prefix && make && make install && cd ..
+n=8
+cd protobuf* && ./configure -prefix=$prefix && make -j$n && make install && cd ..
+cd gflags* && ./configure -prefix=$prefix && make -j$n && make install && cd ..
+cd glog* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
+cd zeromq* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
+cd zlib* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
+cd snappy* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
+# using the following one if you have problem on fortran compiler
+# cd mpich* && ./configure -prefix=$prefix --disable-fortran && make && make -j$n install && cd ..
+cd mpich* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
+cd cityhash* && ./configure -prefix=$prefix && make && make -j$n install && cd ..
 
-mv eigen*/Eigen $prefix/include
+cp -r eigen*/Eigen $prefix/include
 
 cd gtest*
 g++ -isystem ./include -I. -pthread -c ./src/gtest-all.cc
 g++ -isystem ./include -I. -pthread -c ./src/gtest_main.cc
 ar -rv libgtest.a gtest-all.o
 ar -rv libgtest_main.a gtest_main.o
-mv libgtest* ../../lib
-mv ./include/gtest ../../include
+cp libgtest* ../../lib
+cp ./include/gtest ../../include
 cd ..
 
 # clean
